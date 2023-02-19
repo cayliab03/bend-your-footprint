c9ff24d41eaec2eb5b376ebe40d6f06c86a46c58
Caylia Bonnick <117762684+cayliab03@users.noreply.github.com>
2023-02-19 09:31:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,6 @@ import Questions
 
 
 if __name__ == '__main__':
-    print("\033[0;49;31m AAAAAAAAAAAAAAAA\t\033[0;49;91m AAAAAAAAAAAAAAAA\n"
-          "\033[0;49;32m AAAAAAAAAAAAAAAA\t\033[0;49;92m AAAAAAAAAAAAAAAA\n"
-          "\033[0;49;33m AAAAAAAAAAAAAAAA\t\033[0;49;93m AAAAAAAAAAAAAAAA\n"
-          "\033[0;49;34m AAAAAAAAAAAAAAAA\t\033[0;49;94m AAAAAAAAAAAAAAAA\n"
-          "\033[0;49;35m AAAAAAAAAAAAAAAA\t\033[0;49;95m AAAAAAAAAAAAAAAA\n"
-          "\033[0;49;36m AAAAAAAAAAAAAAAA\t\033[0;49;96m AAAAAAAAAAAAAAAA\n"
-          "\033[0;49;37m AAAAAAAAAAAAAAAA\t\033[0;49;97m AAAAAAAAAAAAAAAA\n"
-          "\033[0;49;38m AAAAAAAAAAAAAAAA\t\033[0;49;98m AAAAAAAAAAAAAAAA\n")
-
-
     in_quiz = True  # while var is true, will loop through quiz questions
 
     while in_quiz:  # while quiz is active
